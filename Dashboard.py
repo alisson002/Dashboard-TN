39a3da3a619b471dc3cf0ed311b57daebfe76b24
unknown <alisson-mu@hotmail.com>
2024-03-14 08:57:25 -0300

    atualizando dados e data máxima

--- Dashboard.py
+++ Dashboard.py
@@ -39,7 +39,7 @@ data_minima = pd.to_datetime('2023-01-01')
 if selected_option2 == "Site/Portal":
     data_maxima = pd.to_datetime('2023-10-16')
 else:
-    data_maxima = pd.to_datetime('2024-03-13')
+    data_maxima = pd.to_datetime('2024-03-14')
 #data_maxima = pd.to_datetime(tnPortal.df_noticias.iloc[-1]['not_datapub'])
     
 # Adicionar seletor de períodos na coluna à esquerda
